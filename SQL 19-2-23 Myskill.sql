37a7d17f360530ef60b1c9578c092418705e050d
Elsa Rachel Dementieva <80081804+rachelsa26@users.noreply.github.com>
2023-02-20 01:17:00 +0700

    Add files via upload

---
+++ SQL 19-2-23 Myskill.sql
@@ -0,0 +1,121 @@
+create database db_myskill;
+
+use db_myskill;
+
+CREATE TABLE wisata_jabar_2021(
+   id                  INTEGER 
+  ,kode_provinsi       INTEGER
+  ,nama_provinsi       VARCHAR(10)
+  ,kode_kabupaten_kota INTEGER
+  ,nama_kabupaten_kota VARCHAR(23)
+  ,satuan              VARCHAR(5)
+  ,tahun               INTEGER
+  ,jumlah_pengunjung   INTEGER
+);
+
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (1,32,'JAWA BARAT',3201,'KABUPATEN BOGOR','ORANG',2020,28367);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (2,32,'JAWA BARAT',3202,'KABUPATEN SUKABUMI','ORANG',2020,739368);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (3,32,'JAWA BARAT',3203,'KABUPATEN CIANJUR','ORANG',2020,1071201);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (4,32,'JAWA BARAT',3204,'KABUPATEN BANDUNG','ORANG',2020,1274026);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (5,32,'JAWA BARAT',3205,'KABUPATEN GARUT','ORANG',2020,1907101);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (6,32,'JAWA BARAT',3206,'KABUPATEN TASIKMALAYA','ORANG',2020,658304);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (7,32,'JAWA BARAT',3207,'KABUPATEN CIAMIS','ORANG',2020,616645);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (8,32,'JAWA BARAT',3208,'KABUPATEN KUNINGAN','ORANG',2020,1270574);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (9,32,'JAWA BARAT',3209,'KABUPATEN CIREBON','ORANG',2020,352426);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (10,32,'JAWA BARAT',3210,'KABUPATEN MAJALENGKA','ORANG',2020,375000);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (11,32,'JAWA BARAT',3211,'KABUPATEN SUMEDANG','ORANG',2020,248089);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (12,32,'JAWA BARAT',3212,'KABUPATEN INDRAMAYU','ORANG',2020,210366);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (13,32,'JAWA BARAT',3213,'KABUPATEN SUBANG','ORANG',2020,312063);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (14,32,'JAWA BARAT',3214,'KABUPATEN PURWAKARTA','ORANG',2020,845487);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (15,32,'JAWA BARAT',3215,'KABUPATEN KARAWANG','ORANG',2020,1427075);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (16,32,'JAWA BARAT',3216,'KABUPATEN BEKASI','ORANG',2020,127500);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (17,32,'JAWA BARAT',3217,'KABUPATEN BANDUNG BARAT','ORANG',2020,3463960);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (18,32,'JAWA BARAT',3218,'KABUPATEN PANGANDARAN','ORANG',2020,3940506);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (19,32,'JAWA BARAT',3271,'KOTA BOGOR','ORANG',2020,1284090);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (20,32,'JAWA BARAT',3272,'KOTA SUKABUMI','ORANG',2020,107486);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (21,32,'JAWA BARAT',3273,'KOTA BANDUNG','ORANG',2020,2431290);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (22,32,'JAWA BARAT',3274,'KOTA CIREBON','ORANG',2020,326329);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (23,32,'JAWA BARAT',3275,'KOTA BEKASI','ORANG',2020,42080);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (24,32,'JAWA BARAT',3276,'KOTA DEPOK','ORANG',2020,215466);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (25,32,'JAWA BARAT',3277,'KOTA CIMAHI','ORANG',2020,48177);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (26,32,'JAWA BARAT',3278,'KOTA TASIKMALAYA','ORANG',2020,32875);
+INSERT INTO wisata_jabar_2021(id,kode_provinsi,nama_provinsi,kode_kabupaten_kota,nama_kabupaten_kota,satuan,tahun,jumlah_pengunjung) 
+VALUES (27,32,'JAWA BARAT',3279,'KOTA BANJAR','ORANG',2020,9832);
+
+SELECT * FROM wisata_jabar_2021
+ORDER BY nama_kabupaten_kota DESC;
+
+SELECT * 
+FROM wisata_jabar_2021
+WHERE nama_kabupaten_kota = 'KABUPATEN BOGOR';
+
+SELECT #untuk memilih kolom mana yang ingin ditampilkan
+	id,
+    kode_provinsi,
+    nama_provinsi,
+    nama_kabupaten_kota
+FROM wisata_jabar_2021 #untuk memilih tabel mana yang ingin ditampilkan
+WHERE kode_provinsi = 32; #untuk memfilter data dengan kondisi tertentu
+
+#Total pengunjung provinsi jabar
+SELECT 
+	nama_provinsi,
+    sum(jumlah_pengunjung)
+FROM wisata_jabar_2021
+GROUP BY 1;
+
+SELECT 
+	nama_provinsi,
+    min(jumlah_pengunjung)
+FROM wisata_jabar_2021
+GROUP BY 1;
+
+SELECT 
+	nama_provinsi,
+    max(jumlah_pengunjung)
+FROM wisata_jabar_2021
+GROUP BY 1;
+
+SELECT 
+	nama_provinsi,
+    kode_provinsi,
+    avg(jumlah_pengunjung)
+FROM wisata_jabar_2021
+GROUP BY 1,2;
+
+#Melihat data dalam tabel dengan kondisi values apa pun dalam kolom yang memiliki huruf terakhir "a"
+SELECT * FROM wisata_jabar_2021
+WHERE nama_kabupaten_kota LIKE '%a';
+
+SET SQL_SAFE_UPDATES=0;
+
+UPDATE wisata_jabar_2021
+SET tahun = 2019
+WHERE nama_kabupaten_kota LIKE 'KABUPATEN B%';
+
